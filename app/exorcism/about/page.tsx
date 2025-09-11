ee8399de885b174ab4c9b2210336e47b91c03d90
MakcChernenko <157323123+MakcChernenko@users.noreply.github.com>
2025-09-11 19:01:23 +0300

    added form

--- app/exorcism/about/page.tsx
+++ app/exorcism/about/page.tsx
@@ -1,7 +1,37 @@
+"use client";
+
 import About from "@/Components/Exorcism/ShamanSection/ShamanSection";
+import ContactModal from "@/Components/Modal/ContactModal";
+import { useState } from "react";
 
-function page() {
-  return <About />;
+export default function Page() {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const handleModal = () => {
+    setIsOpenModal(!isOpenModal);
+  };
+
+  return (
+    <div>
+      <About />
+      <ContactModal isOpen={isOpenModal} onClose={handleModal} />
+      <button
+        style={{
+          display: "block",
+          margin: "20px auto",
+          padding: "12px 24px",
+          fontSize: "25px",
+          color: "white",
+          backgroundColor: "#7b2cbf",
+          border: "none",
+          borderRadius: "8px",
+          cursor: "pointer",
+          transition: "background-color 0.3s ease",
+        }}
+        onClick={handleModal}
+      >
+        Замовити ритуал
+      </button>
+    </div>
+  );
 }
-
-export default page;
